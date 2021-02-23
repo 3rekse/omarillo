baadac6995731fb68ea42893caa0b815a5241ea2
Patrick Coxall <mr.coxall@mths.ca>
2021-02-23 14:56:56 -0500

    add placeholder for extension

---
+++ turtle_logo.ts
@@ -0,0 +1,12 @@
+/**
+ * Logo Turtle graphics blocks
+ */
+//% weight=100 color=#0f9c11 icon="\uf188"
+//% groups='[]'
+
+namespace turtle_logo {
+    //% block
+    export function helloWorld() {
+
+    }
+}
