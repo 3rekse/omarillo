22fc1979875aa518fdf65843148a72e87c60c1e7
Patrick Coxall <mr.coxall@mths.ca>
2021-02-24 10:53:55 -0500

    added test.ts code

---
+++ images.g.ts
@@ -0,0 +1,72 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . 7 7 7 . . . 
+. . . . . e e e e . 7 7 f 7 . . 
+. . . . e e e d e e 7 7 7 7 . . 
+. . . . e d e e d e 7 7 7 . . . 
+. . . 7 e e e e e e . . . . . . 
+. . . . 7 7 7 7 7 7 . . . . . . 
+. . . . 7 7 . . 7 7 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . 7 7 7 . . . 
+. . . . . e e e e . 7 7 f 7 . . 
+. . . . e e e d e e 7 7 7 7 . . 
+. . . . e d e e d e 7 7 7 . . . 
+. . . 7 e e e e e e . . . . . . 
+. . . . 7 7 7 7 7 7 . . . . . . 
+. . . . 7 7 . . 7 7 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . e e . . 7 7 7 . . . 
+. . . . . e e e e . 7 7 f 7 . . 
+. . . . e e e d e e 7 7 7 7 . . 
+. . . . e d e e d e 7 7 7 . . . 
+. . . 7 e e e e e e . . . . . . 
+. . . . 7 7 7 7 7 7 . . . . . . 
+. . . . 7 7 . . 7 7 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- test.ts
+++ test.ts
@@ -1,1 +1,24 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let myTurtle = turtle.fromSprite(sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . e e . . 7 7 7 . . .
+    . . . . . e e e e . 7 7 f 7 . .
+    . . . . e e e d e e 7 7 7 7 . .
+    . . . . e d e e d e 7 7 7 . . .
+    . . . 7 e e e e e e . . . . . .
+    . . . . 7 7 7 7 7 7 . . . . . .
+    . . . . 7 7 . . 7 7 . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player))
+myTurtle.setPositionCartesian(-40, 20)
+myTurtle.setSpeed(98)
+for (let index = 0; index < 35; index++) {
+    myTurtle.moveDirection(TurtleDirection.Forward, 75)
+    myTurtle.turnDirectionByDegrees(TurtleTurnDirection.Right, 130)
+}
