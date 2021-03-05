2da2c254e788405125b01a8e0928c1ae6b8e9a7d
Patrick Coxall <mr.coxall@mths.ca>
2021-03-04 22:47:38 -0500

    changed the test.ts code example

--- test.ts
+++ test.ts
@@ -1,24 +1,7 @@
-let myTurtleVariable = turtle.fromSprite(sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . e e . . . . . . . . 
-    . . . . . e e e e . . 7 7 7 . . 
-    . . . . e e e d e e . 7 7 f 7 . 
-    . . . e e e e e d e e 7 7 7 7 . 
-    . . . e e d e e e e e 7 7 7 . . 
-    . . 7 e e e e e e e e . . . . . 
-    . 7 . 7 7 7 7 7 7 7 7 . . . . . 
-    . . . 7 7 7 7 7 7 7 7 . . . . . 
-    . . 7 7 7 . . . . 7 7 7 . . . . 
-    . . 7 7 7 . . . . . 7 7 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player))
-myTurtleVariable.setPositionCartesian(-40, 20)
-myTurtleVariable.setSpeed(98)
-for (let index = 0; index < 35; index++) {
-    myTurtleVariable.moveDirection(TurtleDirection.Forward, 75)
-    myTurtleVariable.turnDirectionByDegrees(TurtleTurnDirection.Right, 130)
+turtle.showTurtle()
+turtle.setPositionCartesian(-40, 20)
+turtle.setSpeed(98)
+for (let index = 0; index < 40; index++) {
+    turtle.moveTurtleDirection(TurtleDirection.Forward, 75)
+    turtle.turnTurtleDirectionByDegrees(TurtleTurnDirection.Right, 130)
 }
