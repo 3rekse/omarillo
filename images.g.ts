537812ffeb2d1f993b5b324686bcd70befc6c419
3rekse <143498061+3rekse@users.noreply.github.com>
2023-12-06 12:19:49 +0100

    Update pxt.json, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -4,29 +4,36 @@ namespace myImages {
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "image1":
-            case "turtleImage":return img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . e e . . . . . . . . 
-    . . . . . e e e e . . 7 7 7 . . 
-    . . . . e e e d e e . 7 7 f 7 . 
-    . . . e e e e e d e e 7 7 7 7 . 
-    . . . e e d e e e e e 7 7 7 . . 
-    . . 7 e e e e e e e e . . . . . 
-    . 7 . 7 7 7 7 7 7 7 7 . . . . . 
-    . . . 7 7 7 7 7 7 7 7 . . . . . 
-    . . 7 7 7 . . . . 7 7 7 . . . . 
-    . . 7 7 7 . . . . 7 7 7 . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . .  
+            case "omarImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 4 . . . . . . . . . 
+. . . . . . 4 . . . . . . . . . 
+. 4 4 . . . 4 4 . . . 4 4 4 . . 
+. 4 . . . 4 4 4 4 . . . . 4 . . 
+. 4 . . 4 4 4 4 4 4 . . . 4 . . 
+. 4 4 4 4 . 4 4 . 4 4 4 4 4 . . 
+. . . 4 4 4 4 4 . 4 4 . . . . . 
+. 4 4 4 4 4 4 4 4 4 4 4 4 . . . 
+. 4 . 4 4 7 7 7 7 4 4 . 4 . . . 
+. . . 4 4 4 4 4 4 4 4 . . . . . 
+. . 4 4 4 . . . . 4 4 4 . . . . 
+. . 4 4 4 . . . . 4 4 4 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 `;
         }
         return null;
     })
 
     helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
         switch(helpers.stringTrim(name)) {
 
         }
