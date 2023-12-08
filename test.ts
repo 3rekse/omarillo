a68e2e80e81367397178f3f2538b2ddd5bbb7407
3rekse <143498061+3rekse@users.noreply.github.com>
2023-12-08 17:42:36 +0100

    test

--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
 omarillo.showOmarillo()
 omarillo.setPositionCartesian(-40, 20)
+omarillo.setPenColor(4)
 omarillo.setSpeed(98)
 for (let index = 0; index < 40; index++) {
     omarillo.moveOmarilloDirection(OmarilloDirection.Forward, 75)
